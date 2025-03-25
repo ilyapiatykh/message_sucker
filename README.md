31e3d164c0820c4debc87807d0d1fac5693b34f0
ilyapiatykh <ilya05072002@gmail.com>
2025-03-25 18:57:48 +0300

    fixes

---
+++ README.md
@@ -0,0 +1,19 @@
+# MESSAGE_SUCKER
+
+## Setup
+
+### Install dependencies
+
+Clone repo and run in project dir:
+
+```bash
+python -m pip install -r requirements.txt
+```
+
+### Create and fill .env file
+
+To get telegram api credentials - <https://core.telegram.org/api/obtaining_api_id>
+
+```bash
+cp .env.example .env
+```
--- main.py
+++ main.py
@@ -1,75 +1,98 @@
+import argparse
+import sys
 from datetime import datetime
+from pathlib import Path
 
-from pydantic import BaseModel, Field, TypeAdapter
+from pydantic import BaseModel, Field, SecretStr, TypeAdapter
 from pydantic_settings import BaseSettings
 from pytz import timezone
 from telethon.sync import TelegramClient
+from tqdm.asyncio import tqdm
 
-TG_SESSION_FILE_PATH = "telethon_session"
+DATA_DIR_NAME = "data"
+TG_SESSION_FILE_NAME = "message_sucker_telethon"
 
 
 class Config(BaseSettings):
-    api_id: str = Field(validation_alias="API_ID")
-    api_hash: str = Field(validation_alias="API_HASH")
-    channel_id: int = Field(validation_alias="CHANNEL_ID")
+    api_id: SecretStr = Field(validation_alias="API_ID")
+    api_hash: SecretStr = Field(validation_alias="API_HASH")
+    channel_id: SecretStr = Field(validation_alias="CHANNEL_ID")
 
 
 class Message(BaseModel):
-    id: str
+    id: int
     text: str | None
     reply_to_msg_id: int | None
     send_time: datetime | None
 
 
-async def get_messages_from_channel(client: TelegramClient, channel_id: int):
+async def get_messages_from_channel(client: TelegramClient, channel_id: int, output: Path):
     offset_date = datetime(year=2025, month=3, day=8, tzinfo=timezone("Europe/Moscow"))
-    limit = 1000
-    wait_time = 2
-    min_id = None
+    wait_time = 3
     messages = []
 
-    while True:
-        for i in client.iter_messages(
+    async for i in tqdm(
+        client.iter_messages(
             entity=channel_id,
-            limit=limit,
             offset_date=offset_date,
-            min_id=min_id,
             wait_time=wait_time,
             reverse=True,
-        ):
-            message = Message(
-                id=i.id,
-                text=i.raw_text,
-                reply_to_msg_id=i.reply_to_msg_id,
-                send_time=i.date,
-            )
+        )
+    ):
+        message = Message(
+            id=i.id,
+            text=i.raw_text,
+            reply_to_msg_id=i.reply_to_msg_id,
+            send_time=i.date,
+        )
 
-            messages.append(message)
+        messages.append(message)
 
-        if len(messages) % 2 != 0:
-            break
-
-        min_id = messages[-1].id
-
-    messages_to_json(messages)
+    messages_to_json(messages, output)
 
 
-def messages_to_json(messages: list[Message]):
+def messages_to_json(messages: list[Message], output: Path):
     json_data = TypeAdapter(list[Message]).dump_json(messages, exclude_unset=True, round_trip=True)
 
-    with open("messages.json", "wb") as f:
+    with open(output, "wb") as f:
         f.write(json_data)
 
 
+def resolve_path():
+    data_dir = Path(__file__).resolve().parent.joinpath(DATA_DIR_NAME)
+    data_dir.mkdir(exist_ok=True)
+
+    return data_dir.joinpath(TG_SESSION_FILE_NAME)
+
+
 def main():
-    config = Config.model_validate()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", type=str)
+
+    args = parser.parse_args()
+    output = Path(args.output).resolve()
+
+    if not output.parent.exists():
+        print("output filepath should ba valid!")
+        sys.exit(1)
+
+    config = Config.model_validate({})
+
+    session = resolve_path()
 
     telethon_client = TelegramClient(
-        TG_SESSION_FILE_PATH, int(config.api_id.get_secret_value()), config.api_hash.get_secret_value()
+        session,
+        int(config.api_id.get_secret_value()),
+        config.api_hash.get_secret_value(),
+        device_model="arbuz phone",  # https://qna.habr.com/q/1232932
+        system_version="Arbuz OS",
+        app_version="1.0.0",
     )
 
     with telethon_client:
-        telethon_client.loop.run_until_complete(get_messages_from_channel(telethon_client))
+        telethon_client.loop.run_until_complete(
+            get_messages_from_channel(telethon_client, int(config.channel_id.get_secret_value()), output)
+        )
 
 
 if __name__ == "__main__":
